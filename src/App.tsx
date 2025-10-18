0879c81fde61b65f1f46f070b9fcb34c40338c33
Nazar-bek <nazarsayfullayev@gmail.com>
2025-10-18 09:39:47 +0500

    Fixed some issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, type FormEvent } from "react";
+import  { useState, type FormEvent } from "react";
 import type { Todo } from "./types";
 import TaskItem from "./components/TaskItem";
 
